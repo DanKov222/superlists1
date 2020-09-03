30a6358c0ed56699d23ed9f95367fc9331197988
DanKov222 <vip.daniil248@gmail.com>
2020-09-03 18:14:35 +0700

    update test and unittest is imported

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,25 @@
 from selenium import webdriver
+import unittest
 
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    """Тест нового пользователя"""
 
-assert 'Django' in browser.title
+    def setUp(self):
+        """Установка браузера"""
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        """Деинсталирование"""
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        """Тест: можно ли начать список и получить его позже"""
+        self.browser.get('http://127.0.0.1:8000/')
+
+        self.assertIn('To-Do lists', self.browser.title)
+        self.fail('Закончить тест!')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
