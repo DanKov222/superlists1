b84d8fc870bcf49683b2e9402bad859ebfdcd9e2
DanKov222 <vip.daniil248@gmail.com>
2020-09-04 17:22:20 +0700

    first unit test and url mapping

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import resolve
+from .views import home_page
 
-# Create your tests here.
+
+class HomePageTest(TestCase):
+    """Тест домашней страницы"""
+
+    def test_root_url_resolves_to_home_page_view(self):
+        """Тест: корневой url преобразуется в представление домашней страницы"""
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
