32a6a2af99e8c5cc7aa54a25b1f6c6d1ed552e21
DanKov222 <vip.daniil248@gmail.com>
2020-09-10 18:41:11 +0700

    new model test saving_and_retrieving_items

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    """Элементы списка"""
+    text = models.TextField(default='')
--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,8 @@ from django.urls import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 
+from lists.models import Item
+
 from .views import home_page
 
 
@@ -27,3 +29,25 @@ class HomePageTest(TestCase):
         """Тест: можно сохранить POST-запрос"""
         response = self.client.get('/', data={'item_text': 'A new list item'})
         self.assertIn('A new list item', response.content.decode())
+
+
+class ItemModelTest(TestCase):
+    """Тест модели элемента списка"""
+
+    def test_saving_and_retrieving_items(self):
+        """Тест: сохранения и получения элементов списка"""
+        first_item = Item
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item
+        second_item.text = 'The second Item'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.text, 'The second Item')
