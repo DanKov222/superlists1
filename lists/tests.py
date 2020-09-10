bfd9de83fb891f9aecebc859e8c67d6e396026ff
DanKov222 <vip.daniil248@gmail.com>
2020-09-10 18:02:00 +0700

    test can save POST request. \n refactored

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from .views import home_page
 
@@ -8,14 +9,21 @@ from .views import home_page
 class HomePageTest(TestCase):
     """Тест домашней страницы"""
 
-    def test_root_url_resolves_to_home_page_view(self):
-        """Тест: корневой url преобразуется в представление домашней страницы"""
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
     def test_home_page_returns_correct_html(self):
         """Тест: домашняя страница возвращает правильный html"""
-        request = HttpRequest()  # То что Django увидит когда пользователь запросит страницу
-        response = home_page(request)
+        response = self.client.get('/')
+
         html = response.content.decode('utf8')
         self.assertIn('<title>To-Do lists</title>', html)
+
+        self.assertTemplateUsed(response, 'lists/home_page.html')    # проверка шаблона
+
+    def test_uses_home_template(self):
+        """Тест: используется домашний шаблон"""
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'lists/home_page.html')
+
+    def test_can_save_a_POST_request(self):
+        """Тест: можно сохранить POST-запрос"""
+        response = self.client.get('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
