842e9542ff9312fb89e9670a51020df7b9c6a7e3
DanKov222 <vip.daniil248@gmail.com>
2020-09-10 17:58:40 +0700

    a new test has been written to check if a string is in the list. refactored

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -13,11 +15,34 @@ class NewVisitorTest(unittest.TestCase):
         """Деинсталирование"""
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        """Подтверждение строки в таблице списка"""
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         """Тест: можно ли начать список и получить его позже"""
         self.browser.get('http://127.0.0.1:8000/')
 
         self.assertIn('To-Do lists', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+
+        inputbox.send_keys('Купить павлиньи перья')    # selenium - input
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        self.check_for_row_in_list_table('1: Купить павлиньи перья')
+        inputbox.send_keys('Сделать мушку')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        self.check_for_row_in_list_table('1: Купить павлиньи перья')
+        self.check_for_row_in_list_table('2: Сделать мушку')
+
         self.fail('Закончить тест!')
 
 
