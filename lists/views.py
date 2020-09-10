ecff17488951f57ac06eb294c86b464982604992
DanKov222 <vip.daniil248@gmail.com>
2020-09-10 18:03:13 +0700

    add PSOT request

--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
 
 def home_page(request):
     """Домашняя страница"""
-    return render(request, 'lists/home_page.html',)
+    return render(request, 'lists/home_page.html', {'new_item_text': request.POST.get('item_text', '')})
 
 
