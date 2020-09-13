9c625f49c4ebe25e7258e08cda07fc15ed2c7487
DanKov222 <vip.daniil248@gmail.com>
2020-09-13 19:24:19 +0700

    test is faster

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,7 +1,11 @@
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import time
 from django.test import LiveServerTestCase
+
+
+MAX_WAIT = 10   # Максимальное время ожидания
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -15,12 +19,19 @@ class NewVisitorTest(LiveServerTestCase):
         """Деинсталирование"""
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        """Подтверждение строки в таблице списка"""
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
-
+    def wait_for_row_in_list_table(self, row_text):
+        """Ожидание элемента в таблице списка"""
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except(AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         """Тест: можно ли начать список и получить его позже"""
@@ -34,15 +45,13 @@ class NewVisitorTest(LiveServerTestCase):
 
         inputbox.send_keys('Купить павлиньи перья')    # selenium - input
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
-        self.check_for_row_in_list_table('1: Купить павлиньи перья')
+        self.wait_for_row_in_list_table('1: Купить павлиньи перья')
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Сделать мушку')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
-        self.check_for_row_in_list_table('1: Купить павлиньи перья')
-        self.check_for_row_in_list_table('2: Сделать мушку')
+        self.wait_for_row_in_list_table('1: Купить павлиньи перья')
+        self.wait_for_row_in_list_table('2: Сделать мушку')
 
         self.fail('Закончить тест!')
