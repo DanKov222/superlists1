226f4ee4e9f0538437ca20d6c57817c763e7af02
DanKov222 <vip.daniil248@gmail.com>
2020-12-28 19:16:10 +0700

    create new model list and update model item

--- lists/models.py
+++ lists/models.py
@@ -4,3 +4,9 @@ from django.db import models
 class Item(models.Model):
     """Элементы списка"""
     text = models.TextField(default='')
+    list = models.ForeignKey('List', default=None, on_delete=models.CASCADE)
+
+
+class List(models.Model):
+    """Списки"""
+    pass
--- lists/tests.py
+++ lists/tests.py
@@ -1,4 +1,4 @@
-from lists.models import Item
+from lists.models import Item, List
 from django.test import TestCase
 
 
@@ -11,18 +11,26 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, 'lists/home_page.html')
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
     """тест модели элемента списка"""
 
     def test_saving_and_retrieving_items(self):
         """тест сохранения и получения элемента списка"""
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -30,7 +38,9 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_)
 
 
 class ListViewTest(TestCase):
@@ -43,9 +53,9 @@ class ListViewTest(TestCase):
 
     def test_display_all_list_item(self):
         """тест: отображает все элементы списка"""
-        # настройка
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
 
         response = self.client.get('/lists/single_list/')
 
