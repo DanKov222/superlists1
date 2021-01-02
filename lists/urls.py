7caf356d368dbc01e7a42213d1d31afb89a6d8c9
DanKov222 <vip.daniil248@gmail.com>
2021-01-02 14:56:04 +0700

    duplicate URL removed

--- lists/urls.py
+++ lists/urls.py
@@ -4,7 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('', home_page, name="home_url"),
-    path('lists/new', new_list, name='new_list_url'),
-    path('lists/<int:list_id>/', view_list, name='view_list'),
-    path('lists/<int:list_id>/add_item', add_item, name='add_item'),
+    path('new', new_list, name='new_list_url'),
+    path('<int:list_id>/', view_list, name='view_list'),
+    path('<int:list_id>/add_item', add_item, name='add_item'),
 ]
