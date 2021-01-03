f7363d94880b8b0a8f485b5cb315b32fa1e07c20
DanKov222 <vip.daniil248@gmail.com>
2021-01-03 18:54:31 +0700

    collectstatic

--- superlists/urls.py
+++ superlists/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from lists import views as list_views
 
+
 urlpatterns = [
     path('', list_views.home_page, name='home'),
     path('lists/', include('lists.urls')),
